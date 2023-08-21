c752f4e148642a15f4c1dffc696871f52a1c6e15
Николай Комаров <nikolajkomarov@MacBook-Air-Nikolaj.local>
2023-08-21 16:52:04 +0300

    test

--- lib/system/utest/hal_system_utmain.cpp
+++ lib/system/utest/hal_system_utmain.cpp
@@ -2,6 +2,7 @@
 
 int main(int ac, char** av)
 {
-    printf("%u %s\n", ac, av);
-    return CommandLineTestRunner::RunAllTests(ac, av);
+    // printf("%u %s\n", ac, av);
+    // return CommandLineTestRunner::RunAllTests(ac, av);
+    RUN_ALL_TESTS(ac, av);
 }
